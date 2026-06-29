c8c7e4e414ba2e3b3285318eba0d8a4448de8dfc
HaseebSiddiqi <Haseebs727@gmail.com>
2026-06-29 01:23:43 -0400

    Added PWA setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 Amplify.configure({
@@ -23,4 +24,7 @@ root.render(
   </React.StrictMode>
 );
 
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
